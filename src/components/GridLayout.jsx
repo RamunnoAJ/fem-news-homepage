8e1aa5908cbd3b07c74b112aa99e10d0e5f3de20
RamunnoAJ <ramunnoagustin@gmail.com>
2022-11-02 14:53:14 -0300

    add Main component

--- src/components/GridLayout.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function GridLayout() {
-  return <div>GridLayout</div>
-}
-
-export default GridLayout
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import Main from '../components/Main'
 
 function Home() {
   return (
     <div>
       <NavBar />
+      <main>
+        <div className='w-11/12 max-w-7xl mx-auto md:grid md:grid-cols-3'>
+          <Main />
+        </div>
+      </main>
     </div>
   )
 }
